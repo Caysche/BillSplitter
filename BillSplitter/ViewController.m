435ef12306a7710c4b6282b437a8930449fbd73a
Caysche <cay.cornelius@gmail.com>
2016-09-17 17:24:04 -0700

    Added finished required tasks for assignment

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *inputBillAmount;
+@property (weak, nonatomic) IBOutlet UISlider *numberOfPeopleSlider;
+@property (weak, nonatomic) IBOutlet UILabel *resultOfCalculation;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeopleLabel;
 
 @end
 
@@ -22,6 +26,22 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)calculateButton:(UIButton *)sender {
+    NSDecimalNumber *billAmount = [NSDecimalNumber decimalNumberWithString:self.inputBillAmount.text];
+    NSDecimalNumber *numberOfPeople = [[NSDecimalNumber alloc] initWithFloat:floorf(self.numberOfPeopleSlider.value)];
+    NSDecimalNumber *resultNumber = [billAmount decimalNumberByDividingBy:numberOfPeople];
+    NSString *resultString = [NSNumberFormatter localizedStringFromNumber: resultNumber numberStyle:NSNumberFormatterCurrencyStyle];
+    self.resultOfCalculation.text = resultString;
+}
+- (IBAction)sliderValueChanged:(UISlider *)sender {
+    [self.numberOfPeopleSlider addTarget:self action:@selector(numberOfPeopleSliderChanged) forControlEvents:UIControlEventValueChanged];
+}
+
+- (float)numberOfPeopleSliderChanged {
+    NSString *labelString = [NSString stringWithFormat:@"%.0f", floor(self.numberOfPeopleSlider.value)];
+    self.numberOfPeopleLabel.text = labelString;
+    return floorf(self.numberOfPeopleSlider.value * 2.0) * 1;
 }
 
 @end
